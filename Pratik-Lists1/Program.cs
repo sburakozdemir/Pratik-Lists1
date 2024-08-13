6c9fa818860ef21edb77006c50fa8ca5f57de78a
sburakozdemir <sburakozdemir00@gmail.com>
2024-08-13 14:26:12 +0300

    Add project files.

---
+++ Pratik-Lists1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Gerekli namespace'i içe aktarıyoruz
+using System.Collections.Generic;
+
+// Ziyaretçilerin isimlerini içeren bir liste oluşturuyoruz
+List<string> ziyaretciler = new List<string>() { "Bülent Ersoy", "Ajda Pekkan", "Ebru Gündeş", "Hadise", "Hande Yener", "Tarkan", "Funda Arar", "Demet Akalın" };
+
+// Ekrana başlık yazdırıyoruz
+Console.WriteLine("** Ziyaretçiler **");
+
+// For döngüsü ile listedeki her ziyaretçiyi yazdırıyoruz
+for (int i = 0; i < ziyaretciler.Count; i++)
+{
+    // i+1 ile 1'den başlayan sıra numarası ekliyoruz
+    // ziyaretciler[i] ile listedeki her ismi alıyoruz
+    Console.WriteLine(i + 1 + "-" + ziyaretciler[i]);
+}
+
